588e0e40c81bc570b36c3a604fa0067363c590d7
Brock <brock@LAPTOP-EO3G02HJ.localdomain>
2023-01-17 16:46:31 -0700

    Object

---
+++ app.js
@@ -0,0 +1,44 @@
+// Object Destructing 1
+// 8 and 1846
+
+// Object Destructuring 2
+// yeaNeptuneDiscovered: 1846, yearMarsDiscovered: 1659
+
+// Object Destructuring 3
+// Your name is Alejandro and you like purple
+// Your name is Melissa and you like green
+// Your name is undefined and you like green
+
+// Array Destructuring 1
+// 'Maya'
+// 'Marisa'
+// 'Chi'
+
+// Array Destructuring 2
+//  "Raindrops on roses",
+// "whiskers on kittens",
+// ["Bright copper kettles", "warm woolen mittens", "Brown paper packages tied up with strings"]
+
+
+// Array Destructuring 3
+// [10,30,20]
+
+// ES5 Assigning Variables to Object Properties
+
+// const obj = {
+//     numbers: {
+//       a: 1,
+//       b: 2
+//     }
+//   };
+  
+//   var a = obj.numbers.a;
+//   var b = obj.numbers.b
+
+// const {a,b} = obj.numbers
+
+// ES5 Array Swap
+// [arr[0], arr[1]] = [arr[1], arr[0]]
+
+// raceResults
+// const raceResults= ([first,second, third, ...rest]) => ({first, second,third,rest})
